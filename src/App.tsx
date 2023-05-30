422ba7ddcc995bc29df1eef5c7e5b8930ad63bab
Yair Morgenstern <yairm210@hotmail.com>
2023-05-30 19:38:34 +0300

    Add CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
